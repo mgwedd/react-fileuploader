a0fcb8dc7ad714fc87490a76661862e2c4bd2c75
mgwedd <michael.wedd@gmail.com>
2019-05-11 13:59:54 -0400

    got components and props and state mgmt wired up. added some tests.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
+import FilterableList from './FilterableList/FilterableList'; 
+import SearchBar from './SearchBar/SearchBar';
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+      filterOption: 'All'
+    };
+  }
+
+  updateSearchTerm(term) {
+    this.setState({
+      searchTerm: term
+    })
+  }
+
+  updateFilterOption(option) {
+    this.setState({
+      filterOption: option
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>YOUR APPLICATION NAME!</h1>  
+        <SearchBar
+          searchTerm={this.state.searchTerm}
+          filterOption={this.state.filterOptions}
+          handleUpdate={term => this.updateSearchTerm(term)} 
+          handleFilterChange={option => this.updateFilterOption(option)} />
+         <FilterableList
+          files={this.props.files}  
+          searchTerm={this.state.searchTerm}
+          filterOption={this.state.filterOption} />
       </div>
     );
   }
 }
-
-export default App;
---
+++ src/FilterableList/FilterableList.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './FilterableList.css';
+import ListItem from '../ListItem/ListItem';
+
+export default class FilterableList extends Component {
+  render() {
+    const { searchTerm, filterOption } = this.props;
+    const list = this.props.files
+        .filter(file => file.name.includes(searchTerm)
+              && (filterOption === 'All' || file.status === filterOption))
+        .map((file, key) => <ListItem {...file} key={key} />);
+    return (
+      <div className="FilterableList">
+        {list} 
+      </div>
+    );
+  }
+}
+
+FilterableList.defaultProps = {
+  files: []
+};
